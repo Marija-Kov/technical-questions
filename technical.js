12ff678cfaa76661ec0d923b7e805ed69e5d152c
Marija K <kccmrj@outlook.com>
2022-06-14 22:14:28 +0200

    Update technical.js

--- technical.js
+++ technical.js
@@ -1,22 +1,18 @@
 import fetch from "node-fetch";
 
-let url = "data.json"; //this is somehow invalid ! 
+let url = 'data.json'; //this is somehow invalid ! WHY ?
+
 async function getRandomQuestion() {
+
     const response = await fetch(url);
-    const data = response.json();
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
     console.log(data)
-//   try {
-//     const response = await fetch(url);
-
-//     if (!response.ok) {
-//       throw new Error(`Error! status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     console.log(data)
-//   } catch (err) {
-//     console.log(err);
-//   }
+  
 
 }
 
