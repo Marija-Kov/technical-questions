3ead4ffe8c0eb6ab728fcb236abac97608d194f3
Marija K <kccmrj@outlook.com>
2022-06-15 09:57:48 +0200

    update

--- technical.js
+++ technical.js
@@ -1,5 +1,8 @@
+//the app should get a random question
+//the user should be able to pick the category from which the question is drawn (general CS, javascript, css, html, web), or get a random question without a category specified
+ 
 
-    
+//the user should be able to put aside the questions that they don't want to come up again 
 async function getRandomQuestion() {
 
     const response = await fetch('data.json');
@@ -8,14 +11,16 @@ async function getRandomQuestion() {
       throw new Error(`Error! status: ${response.status}`);
     }
 
-    const data = await response.text();
+    const data = await response.json();
+    
     console.log(data)
   
 
 }
 
-getRandomQuestion(); // this WORKS in the browser console with the live server - I get the data as I wanted it. 
+getRandomQuestion(); 
+// fetch WORKS in the browser console with the live SERVER - I get the data as I wanted it. 
 //HOWEVER, in the VS integrated terminal, it first requires fetch to be installed (npm init -y ; npm install node-fetch ; add "type":"module" to package.json ; import fetch from "node-fetch" in the js file where fetch is being used -- this works perfectly for online API data, but not for local json file)
 //after fetch has been installed and implemented, it throws TypeError [ERR_INVALID_URL]: Invalid URL.....  input: 'data.json', code: 'ERR_INVALID_URL'
-//Conclusion: I suspect that, to fetch from a local file and get data not using browser + live server, it would be necessary to set up a local server somehow. 
+//Conclusion: To fetch from a local file and get data not using browser + live server, it would be necessary to set up a local server somehow. 
 //All of the above commented would be an issue if the app was to be offline. Also, for the purpose of an offline app, the JSON file could be converted to .js
