47fd9b02262fb75b3b541a4f7666bf63f6339d8c
Marija K <kccmrj@outlook.com>
2022-06-17 23:29:29 +0200

    update

--- technical.js
+++ technical.js
@@ -1,31 +1,44 @@
 //the app should get a random question
 //the user should be able to pick the category from which the question is drawn (general CS, javascript, css, html, web), or get a random question without a category specified
 //GETTING A RANDOM QUESTION OUT OF A RANDOM CATEGORY
-
 //the user should be able to put aside the questions that they don't want to come up again 
+let showAnswer = document.querySelector('.show-answer');
+let nextQuestion = document.querySelector(".next-random");
+let question = document.querySelector(".question");
+let answer = document.querySelector(".answer");
+
+nextQuestion.addEventListener('click', getRandomQuestion);
+showAnswer.addEventListener('click', () => {
+  answer.setAttribute('style', "height: fit-content");
+})
+
 async function getRandomQuestion() {
 
-    const response = await fetch('test.json');
+    const response = await fetch('data.json');
 
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     }
 
-    const test = await response.json();
-    
-    console.log(test[0])
+    const data = await response.json();
+    const randomQuestion = await data[Math.round(Math.random()*96)];
+    question.innerText = `${randomQuestion.q}`;
+    answer.setAttribute('style', "height: 0");
+    answer.innerText = `${randomQuestion.a}`;
   
-
 }
-
-function asciiConverter(string) {
-  for (let i = 0; i < string.length; ++i){
-  return string.split("").map(letter => letter.charCodeAt(i));
-  }
-}
-console.log(asciiConverter("buzz"))
 
 getRandomQuestion(); 
+
+
+// function asciiConverter(string) {
+//   for (let i = 0; i < string.length; ++i){
+//   return string.split("").map(letter => letter.charCodeAt(i));
+//   }
+// }
+// //console.log(asciiConverter("buzz"))
+
+
 // fetch WORKS in the browser console with the live SERVER - I get the data as I wanted it. 
 //HOWEVER, in the VS integrated terminal, it first requires fetch to be installed (npm init -y ; npm install node-fetch ; add "type":"module" to package.json ; import fetch from "node-fetch" in the js file where fetch is being used -- this works perfectly for online API data, but not for local json file)
 //after fetch has been installed and implemented, it throws TypeError [ERR_INVALID_URL]: Invalid URL.....  input: 'data.json', code: 'ERR_INVALID_URL'
